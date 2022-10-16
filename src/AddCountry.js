9287ea27397061b74c9e8980b73b19cd80a31398
Igor Zheleznov <igorzheleznov@gmail.com>
2022-10-16 19:52:47 +0300

    add graphql

--- src/AddCountry.js
+++ src/AddCountry.js
@@ -1,8 +1,32 @@
+import { useLazyQuery, gql } from "@apollo/client";
+import { useRef } from "react";
+
+const GET_COUNTRY = gql`
+    query Country($code: ID!) {
+        country(code: $code) {
+            code
+            name
+            currency
+            phone
+        }
+    }
+`;
+
 export default function AddCountry() {
+    const [getCountry, { loading, error, data }] = useLazyQuery(GET_COUNTRY);
+    const inputRef = useRef();
+
     return (
         <div>
-            <input value="test" />
-            <button>Add</button>
+            <input ref={inputRef} />
+            <button
+                onClick={() =>
+                    getCountry({ variables: { code: inputRef.current.value } })
+                }
+            >
+                Add
+            </button>
+            {data?.country?.name}
         </div>
     );
 }
